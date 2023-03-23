218e29dd9caaef4873383fa22a8e67c7e0cd02e8
aantarctica <mcgulapa@up.edu.ph>
2023-03-23 21:11:44 +0800

    Added Oh Vue Icons

--- src/main.js
+++ src/main.js
@@ -2,4 +2,16 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { OhVueIcon, addIcons } from "oh-vue-icons"
+import * as BiIcons from "oh-vue-icons/icons/bi"
+import * as RiIcons from "oh-vue-icons/icons/ri"
+
+const Bi = Object.values({ ...BiIcons });
+addIcons(...Bi);
+
+const Ri = Object.values({ ...RiIcons });
+addIcons(...Ri);
+
+const app = createApp(App);
+app.component("v-icon", OhVueIcon);
+app.mount("#app");
